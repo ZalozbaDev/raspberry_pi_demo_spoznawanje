8ddf7b3c849a4ce986a5cc18749d44afb92f342d
Daniel Zoba <daniel.sobe@sorben.com>
2021-08-25 14:02:21 +0200

    use new functionality 'open sound card by name'

--- startme.sh
+++ startme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /dLabPro/bin.release/
 
-# sound card position may be different!
-./recognizer -cfg recognizer.cfg -out vad -d 4
+# sound card now defined in config (by name)
+./recognizer -cfg recognizer.cfg -out vad
