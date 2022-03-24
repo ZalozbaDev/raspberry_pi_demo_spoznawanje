818642bbe13c5c52eb6fdf2f340308b4be1500f5
Daniel Zoba <daniel.sobe@sorben.com>
2022-03-24 16:28:33 +0100

    have two configs for two different microphones

--- startme.sh
+++ startme.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /dLabPro/bin.release/
 
+rm -f recognizer.cfg
+cp recognizer_base.cfg recognizer.cfg
+
+echo "audio.name = $PORTAUDIO_SOUND_CARD_NAME"     >> recognizer.cfg
+echo "vad.type   = $VOICE_ACTIVITY_DETECTION_ALGO" >> recognizer.cfg
+
 # sound card now defined in config (by name)
 ./recognizer -cfg recognizer.cfg -out vad | grep -v pF
