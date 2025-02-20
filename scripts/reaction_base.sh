d60f438870eec595f415ea8e6fc60cbab303a219
Daniel Zoba/Sobe <d.zoba@zalozba.de>
2025-02-20 16:33:57 +0100

    add joke reaction

--- scripts/reaction_base.sh
+++ scripts/reaction_base.sh
@@ -33,7 +33,7 @@ else
 			;;
 		*)
 			echo "Reject reaction for active states"
-			mplayer -ao $AUDIOOUTPUTDEVICE $(shuf -n1 -e njejsym_rozumil.mp3 prosu_hisce_raz.mp3)
+			mplayer -ao $AUDIOOUTPUTDEVICE $(shuf -n1 -e to_njejsom_rozmel.mp3 psosym_hysci_raz.mp3)
 			;;
 	    esac
 		;;
@@ -108,6 +108,9 @@ else
 		python3 ./ring_status.py SLEEP
 		python3 ./snooze.py
 		mplayer -ao $AUDIOOUTPUTDEVICE slysu.mp3
+		;;
+	_JOKE_*)
+		mplayer -ao $AUDIOOUTPUTDEVICE zort_2.mp3
 		;;
 	*)
 		echo "$1 is unknown!"
